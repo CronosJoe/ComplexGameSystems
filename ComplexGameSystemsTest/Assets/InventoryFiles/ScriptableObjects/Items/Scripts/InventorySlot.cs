63fc03a99b9656e9a376c486e9d8cca9ce4c1edc
CronosJoe <CronosJoe@gmail.com>
2022-01-13 08:48:04 -0800

    should've added this last time

---
+++ ComplexGameSystemsTest/Assets/InventoryFiles/ScriptableObjects/Items/Scripts/InventorySlot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySlot
+{
+    public ItemObject item;
+    public int amount;
+    public InventorySlot(ItemObject _item, int _amount) 
+    {
+        item = _item;
+        amount = _amount;
+    }
+    public void AddAmount(int value) 
+    {
+        amount += value;
+    }
+}
---
+++ ComplexGameSystemsTest/Assets/InventoryFiles/ScriptableObjects/Items/Scripts/ItemObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum ItemType //I will be providing base types of items you can expand upon this list for your game
+{
+    Food,
+    Equipment,
+    Default
+}
+public abstract class ItemObject : ScriptableObject
+{
+    public GameObject prefab;
+    public ItemType type; //storing this items type
+    [TextArea(15, 20)]
+    public string description; //storing the items description
+    
+}
